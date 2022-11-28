ba65de0ff2206a1b4f76e85abdcb813e48b1f8c7
Shriramu2002 <shriramu2002@gmail.com>
2022-11-28 12:40:24 +0530

    9th submission

--- myApp.js
+++ myApp.js
@@ -100,8 +100,12 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name:personName},{$set:{age:ageToSet}},{new:true},function(err,data){
+    if (err) 
+    return done(err);
+    done(null, data);
+  });
+  
 };
 
 const removeById = (personId, done) => {

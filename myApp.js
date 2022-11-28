640762b3b2c4ba5ea793bd4b8899468383a1182d
Shriramu2002 <shriramu2002@gmail.com>
2022-11-28 13:10:22 +0530

    12th submission

--- myApp.js
+++ myApp.js
@@ -128,8 +128,12 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({favoriteFoods:foodToSearch}).sort({name:1}).limit(2).select({age:0}).exec(function(err,data){
+    if (err) 
+    return done(err);
+    done(null, data);
+  })
+  
 };
 
 /** **Well Done !!**

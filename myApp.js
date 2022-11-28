faa36e61e72fc974e6e327ab6b0c60b8b322621f
Shriramu2002 <shriramu2002@gmail.com>
2022-11-28 12:55:19 +0530

    10th submission

--- myApp.js
+++ myApp.js
@@ -109,7 +109,11 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId,function(err,data){
+    if (err) 
+    return done(err);
+    done(null, data);
+  });
 };
 
 const removeManyPeople = (done) => {
